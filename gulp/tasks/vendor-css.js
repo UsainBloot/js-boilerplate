08b8d57d9d66155fa36e02d3f0b37d467505c24c
Jack Palmer <jackpalmer2@gmail.com>
2016-01-12 09:38:39 +0000

    Updated npm dependencies

--- gulp/tasks/vendor-css.js
+++ gulp/tasks/vendor-css.js
@@ -3,14 +3,14 @@
 var config = require('../config');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
+var cssNano = require('gulp-cssnano');
 
 gulp.task('vendor-css', function() {
 
   return gulp
     .src(config.vendor.css.src)
     .pipe(concat(config.vendor.css.file))
-    .pipe(minifyCSS({
+    .pipe(cssNano({
       compatibility: 'ie8'
     }))
     .pipe(gulp.dest(config.vendor.css.dest));
