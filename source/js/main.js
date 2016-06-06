5f94e508d99aa399784752dbc4fadec5d606cb6b
Jack Palmer <jackpalmer2@gmail.com>
2016-06-06 14:44:43 +0100

    Fixing broken tests by removing waiting for dom elements

--- source/js/main.js
+++ source/js/main.js
@@ -5,16 +5,6 @@
   window.app = {};
   window.app.components = {};
 
-  var pageName = document.querySelector('[data-page]').getAttribute('data-page');
-
-  switch(pageName) {
-    case 'home':
-      require("./pages/page");
-    break;
-
-    default:
-      console.error('No page type found');
-    break;
-  }
+  require("./pages/page");
 
 })();
