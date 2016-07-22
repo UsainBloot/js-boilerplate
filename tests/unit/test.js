b792dc6b32e09ae5b983a700d902b864a86d822a
Jack Palmer <UsainBloot@users.noreply.github.com>
2016-07-22 11:00:49 +0100

    Update test.js

--- tests/unit/test.js
+++ tests/unit/test.js
@@ -4,7 +4,4 @@ describe("App JS Object", function() {
   it('should be defined', function() {
     expect(window.app).to.not.be.undefined;
   });
-  it('should have data', function() {
-    expect(window.app.data).to.eql(testData.test1);
-  });
 });
